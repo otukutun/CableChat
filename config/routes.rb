a4853d7c96327d9c98a8df5842f918db766a6323
otukutun <orehaorz@gmail.com>
2016-12-24 11:46:33 +0900

    Create ChatRoom.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :chat_rooms, except: %i(delete update edit)
+  resources :chat_rooms, only: %i(new create show index)
+  root 'chat_rooms#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
