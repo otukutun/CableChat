ed3c106b3b0c3ef3cc0ffbd95b533af174145cf1
otukutun <orehaorz@gmail.com>
2016-12-24 12:49:30 +0900

    Gonyo gonyo.

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -18,7 +18,8 @@ class ChatRoomsController < ApplicationController
   end
 
   def show
-      @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    @message = Message.new
   end
 
   private
